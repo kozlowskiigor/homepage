4b34ac755bc10a5fa671c628f014e33b6fa592ec
Igor Kozlowski <igor3648@gmail.com>
2022-09-21 02:18:59 +0200

    functions improved

--- index2/script2.js
+++ index2/script2.js
@@ -1,64 +1,80 @@
-const welcome = () => {
-    console.log("Cześć programisto! Witam na stronie opisującej po krótce moje życie.");
-}
+{
+    const welcome = () => {
+        console.log("Cześć programisto! Witam na stronie opisującej po krótce moje życie.");
+    };
 
-const togglePageTheme = () => {
-    body.classList.toggle("lightstyle");
-    table.classList.toggle("darkStyle");
-    if (body.classList.contains("lightstyle")) {
-        changingBackground.innerText = "Zmień motyw na ciemny";
-    } else {
-        changingBackground.innerText = "Zmień motyw na jasny";
-    }
-    headings.forEach((heading) => {
-        heading.classList.toggle("heading--darker");
-    })
-    everyButton.forEach((button) => {
-        button.classList.toggle("darkStyle");
-        button.classList.toggle("whiteFont");
-    })
-}
+    const togglePageTheme = () => {
+        body.classList.toggle("lightstyle");
+        table.classList.toggle("darkStyle");
+        if (body.classList.contains("lightstyle")) {
+            changingBackground.innerText = "Zmień motyw na ciemny";
+        } else {
+            changingBackground.innerText = "Zmień motyw na jasny";
+        }
+        headings.forEach((heading) => {
+            heading.classList.toggle("heading--darker");
+        })
+        everyButton.forEach((button) => {
+            button.classList.toggle("darkStyle");
+            button.classList.toggle("whiteFont");
+        })
+    };
 
-welcome();
+    const body = document.querySelector(".body");
+    const table = document.querySelector(".table");
+    const changingBackground = document.querySelector(".js-changingBackground");
+    const headings = document.querySelectorAll(".heading");
+    const everyButton = document.querySelectorAll(".button");
 
-const body = document.querySelector(".body");
-const buttonHideMyPhoto = document.querySelector(".js-buttonHideMyPhoto");
-const buttonHideTeamPhoto = document.querySelector(".js-buttonHideTeamPhoto");
-const buttonHideLijaPhoto = document.querySelector(".js-buttonHideLijaPhoto");
-const changingBackground = document.querySelector(".js-changingBackground");
-const everyButton = document.querySelectorAll(".button");
-const table = document.querySelector(".table");
-const myPicture = document.querySelector(".myPicture");
-const wks = document.querySelector(".wks");
-const lija = document.querySelector(".lija");
-const headings = document.querySelectorAll(".heading");
+    changingBackground.addEventListener("click", togglePageTheme);
 
-// Ukryj/Pokaż zdjęcie 
-buttonHideMyPhoto.addEventListener("click", () => {
-    myPicture.classList.toggle("hidePhoto");
-    if (myPicture.classList.contains("hidePhoto")) {
-        buttonHideMyPhoto.innerText = "Pokaż moje zdjęcie";
-    } else {
-        buttonHideMyPhoto.innerText = "Ukryj moje zdjęcie";
-    }
-});
+    welcome();
+};
 
-buttonHideTeamPhoto.addEventListener("click", () => {
-    wks.classList.toggle("hidePhoto");
-    if (wks.classList.contains("hidePhoto")) {
-        buttonHideTeamPhoto.innerText = "Pokaż zdjęcie drużyny";
-    } else {
-        buttonHideTeamPhoto.innerText = "Ukryj zdjęcie drużyny";
-    }
-});
+{
+    const hideMyPhoto = () => {
+        myPicture.classList.toggle("hidePhoto");
+        if (myPicture.classList.contains("hidePhoto")) {
+            buttonHideMyPhoto.innerText = "Pokaż moje zdjęcie";
+        } else {
+            buttonHideMyPhoto.innerText = "Ukryj moje zdjęcie";
+        }
+    };
 
-buttonHideLijaPhoto.addEventListener("click", () => {
-    lija.classList.toggle("hidePhoto");
-    if (lija.classList.contains("hidePhoto")) {
-        buttonHideLijaPhoto.innerText = "Pokaż zdjęcie pjeska";
-    } else {
-        buttonHideLijaPhoto.innerText = "Ukryj zdjęcie pjeska";
-    }
-});
+    const buttonHideMyPhoto = document.querySelector(".js-buttonHideMyPhoto");
+    const myPicture = document.querySelector(".myPicture");
 
-changingBackground.addEventListener("click", togglePageTheme);
+    buttonHideMyPhoto.addEventListener("click", hideMyPhoto)
+};
+
+{
+    const hideTeamPhoto = () => {
+        wks.classList.toggle("hidePhoto");
+        if (wks.classList.contains("hidePhoto")) {
+            buttonHideTeamPhoto.innerText = "Pokaż zdjęcie drużyny";
+        } else {
+            buttonHideTeamPhoto.innerText = "Ukryj zdjęcie drużyny";
+        }
+    };
+
+    const buttonHideTeamPhoto = document.querySelector(".js-buttonHideTeamPhoto");
+    const wks = document.querySelector(".wks");
+
+    buttonHideTeamPhoto.addEventListener("click", hideTeamPhoto)
+};
+
+{
+    const hideLijaPhoto = () => {
+        lija.classList.toggle("hidePhoto");
+        if (lija.classList.contains("hidePhoto")) {
+            buttonHideLijaPhoto.innerText = "Pokaż zdjęcie pjeska";
+        } else {
+            buttonHideLijaPhoto.innerText = "Ukryj zdjęcie pjeska";
+        }
+    };
+
+    const buttonHideLijaPhoto = document.querySelector(".js-buttonHideLijaPhoto");
+    const lija = document.querySelector(".lija");
+
+    buttonHideLijaPhoto.addEventListener("click", hideLijaPhoto)
+};
